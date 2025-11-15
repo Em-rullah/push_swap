354a9b763ad8c16237548f26cbc92b7aa33036ac
Emrullah Kir <emkir@k1m02s03.42istanbul.com.tr>
2025-11-15 16:06:15 +0300

    first

--- utils_four.c
+++ utils_four.c
@@ -3,22 +3,31 @@
 /*                                                        :::      ::::::::   */
 /*   utils_four.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: emkir <emkir@student.42.fr>                +#+  +:+       +#+        */
+/*   By: emkir <emkir@student.42istanbul.com.tr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/12 12:42:06 by emkir             #+#    #+#             */
-/*   Updated: 2025/11/13 11:08:50 by emkir            ###   ########.fr       */
+/*   Updated: 2025/11/15 16:01:52 by emkir            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+static void	catch_error_split(char *msg, t_stack *a,
+						t_stack *b, char ***argv_two)
+{
+	while (*msg)
+		write(2, msg++, 1);
+	write(2, "\n", 1);
+	clean_a(a);
+	clean_b(b);
+	free_words(*argv_two);
+	exit(EXIT_FAILURE);
+}
+
 static void	check_ft_atoi(long n, t_stack *a, t_stack *b, char ***argv_two)
 {
 	if (n > INT_MAX || n < INT_MIN)
-	{
-		free_words(*argv_two);
-		catch_error("Error", a, b);
-	}
+		catch_error_split("Error", a, b, argv_two);
 }
 
 int	ft_atoi_split(const char *nptr, t_stack *a, t_stack *b, char ***argv_two)
@@ -36,7 +45,7 @@ int	ft_atoi_split(const char *nptr, t_stack *a, t_stack *b, char ***argv_two)
 		if (nptr[i++] == '-')
 			sign = -sign;
 	if (!nptr[i])
-		catch_error("Error", a, b);
+		catch_error_split("Error", a, b, argv_two);
 	while ('0' <= nptr[i] && nptr[i] <= '9')
 	{
 		n = 10 * n + (nptr[i++] - '0');
